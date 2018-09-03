8130082d31dbbd146d8d27454e16826f11ec027f
Victor Florintsev <victorflorintsev1996@gmail.com>
2018-09-02 19:12:57 -0500

    added selfAttackExceptionTest to testSuite

--- src/test/java/TestSuiteExample.java
+++ src/test/java/TestSuiteExample.java
@@ -5,4 +5,19 @@ public class TestSuiteExample {
     public void test1() {
         assert 1 == 1;
     }
+
+    @Test
+    public void selfAttackExceptionTest() {
+        Territory britain = new Territory();
+        Exception test = null;
+
+        try {
+            britain.attack(britain);
+        } catch (Territory.SelfAttackException e) {
+            test = e;
+            System.out.println("You may not attack yourself, test successful");
+        }
+
+        assert test instanceof SelfAttackException;
+    }
 }
