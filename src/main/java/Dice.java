f579fdb5ab3a22ffc593da4fb4ca71a56aa5f420
evelyngalvan <lgalvanevelyn@gmail.com>
2018-09-08 21:08:05 -0500

    Dice design/implementation update

---
+++ src/main/java/Dice.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Dice implements Comparable<Dice>{
+    public Dice() {}
+
+    protected List<Integer> diceFaces;
+
+    public Dice roll(Integer numDiceToRoll) {
+        diceFaces = new ArrayList<>();
+        for (int i = 0; i < numDiceToRoll; i++) {
+            diceFaces.add((int)(Math.random()*6+1));
+        }
+        return this;
+    }
+
+    @Override
+    public int compareTo(Dice other) {
+        return this.getMax() - other.getMax();
+    }
+
+    private Integer getMax() {
+        return Collections.max(diceFaces);
+    }
+}
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,4 +3,13 @@ import java.util.List;
 public class Player {
     String name;
     List<RiskCard> hand;
+
+    public void attack(Territory attackingTerritory, Territory defendingTerritory){
+        //TODO: check if 'attackingTerritory' is bordering 'defendingTerritory', if not throw exception (@Victor's idea)
+
+//        this.roll() // our roll
+//        defendingTerritory.controlledBy.roll() // other person's roll
+        // ToDo: compare rolls - Victor
+
+    }
 }
