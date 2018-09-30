259af337bab65dcbf70efc4db49496459e71d8cb
Navin Ghimire <ghimire.navin@gmail.com>
2018-09-30 17:10:40 -0500

    Added MoveTroops command class with test cases

---
+++ src/main/java/riskgame/gameobject/MoveTroopsToEnemy.java
@@ -0,0 +1,7 @@
+package riskgame.gameobject;
+
+public class MoveTroopsToEnemy extends MoveTroops {
+    public MoveTroopsToEnemy(Territory newOriginTerritory, Territory newDestinationTerritory, int numArmies) {
+        super(newOriginTerritory, newDestinationTerritory, numArmies);
+    }
+}
--- src/test/java/riskgame/CommandTest.java
+++ src/test/java/riskgame/CommandTest.java
@@ -3,6 +3,9 @@ package riskgame;
 import org.testng.annotations.Test;
 import riskgame.commands.Command;
 import riskgame.commands.CommandManager;
+import riskgame.gameobject.MoveTroops;
+import riskgame.gameobject.MoveTroopsToEnemy;
+import riskgame.gameobject.MoveTroopsToOwn;
 import riskgame.gameobject.player.Player;
 import riskgame.gameobject.Territory;
 
@@ -95,5 +98,43 @@ public class CommandTest {
         assertTrue(territoryA.getArmies() == numArmiesA);
         assertTrue(territoryD.getArmies() == numArmiesD);
         assertTrue(territoryD.getControlledBy() == defender);
+    }
+    @Test
+    public void MoveTroopsTest() throws Exception {
+        Player attacker = new Player();
+        Player defender = new Player();
+        int armiesToMove = 2;
+        Territory territoryA = new Territory(attacker, "Argentina");
+        int numArmiesA = 5;
+        territoryA.addArmies(numArmiesA);
+        Territory territoryD = new Territory(defender, "Denmark");
+        int numArmiesD = 4;
+        territoryD.addArmies(numArmiesD);
+
+        Territory territoryC = new Territory(attacker, "China");
+        int numArmiesC = 3;
+        territoryC.addArmies(numArmiesC);
+
+        Command command1 = new MoveTroopsToEnemy(territoryA,territoryD,armiesToMove);
+        CommandManager commandManager = new CommandManager();
+        commandManager.executeCommand(command1);
+        assertTrue(territoryA.getArmies() == numArmiesA-armiesToMove);
+        assertTrue(territoryD.getArmies() == numArmiesD+armiesToMove);
+
+        Command command2 = new MoveTroopsToOwn(territoryA,territoryC,armiesToMove);
+        commandManager = new CommandManager();
+        commandManager.executeCommand(command2);
+        assertTrue(territoryA.getArmies() == 1);
+        assertTrue(territoryC.getArmies() == 5);
+        System.out.println(territoryA.getArmies());
+        System.out.println(territoryD.getArmies());
+        System.out.println(territoryC.getArmies());
+        commandManager.undo();
+        System.out.println(territoryA.getArmies());
+        System.out.println(territoryD.getArmies());
+        System.out.println(territoryC.getArmies());
+
+
+
     }
 }
