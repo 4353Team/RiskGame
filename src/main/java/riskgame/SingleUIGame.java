4c3be68e4c5e52b5364b8c2644f190d2d6deb759
victorflorintsev <victorflorintsev1996@gmail.com>
2018-10-01 21:38:53 -0500

    I just did.... a LOT... we'll talk about this tomorrow guys

---
+++ src/main/java/riskgame/SingleUIGame.java
@@ -0,0 +1,196 @@
+package riskgame;
+
+import riskgame.commands.Command;
+import riskgame.commands.CommandManager;
+import riskgame.gameobject.Territory;
+import riskgame.gameobject.player.Player;
+import riskgame.ui.UI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleUIGame implements GameEngine {
+    private final CommandManager commandManager = new CommandManager();
+    private UI ui;
+    private List<Territory> territories;
+    private GameState gameState;
+    private Player currentPlayer;
+    private List<Player> playerOrderList = new ArrayList<>();
+    public SingleUIGame() {
+    }
+
+    @Override
+    public void addUi(UI ui) {
+        this.ui = ui;
+    }
+
+    @Override
+    public void start() throws Exception {
+        gameState = GameState.SELECT_MAP;
+        boolean exit = false;
+        while (!exit) {
+            try {
+                switch (gameState) {
+                    case SELECT_MAP:
+                        GameMaps.GameMap selectedGameMap = ui.selectMap(GameMaps.instanceOf());
+                        Command selectMap = new SelectMap(this, selectedGameMap);
+                        commandManager.executeCommand(selectMap);
+                        break;
+                    case SELECT_PLAYERS:
+                        List<Player> list = ui.selectAndNamePlayers();
+                        Command setPlayers = new SetPlayers(this, list);
+                        commandManager.executeCommand(setPlayers);
+                        break;
+                    case INIT_DRAFT:
+                        // not finished - this is a test essentially
+                        int armiesDrafted = 0;
+                        while (armiesDrafted < 50) {
+                            Territory territory = ui.getInitDraftPick(currentPlayer);
+                            Command draftOneInit = new DraftOneInit(this, territory, currentPlayer);
+                            commandManager.executeCommand(draftOneInit); // selects the next player in the command as
+                            // well
+                            armiesDrafted++;
+                        }
+                        gameState = GameState.END;
+                        break;
+                    case END:
+                        exit = true;
+                        break;
+                }
+            } catch (Exception e) {
+                ui.error(e);
+                throw e;
+            }
+        }
+    }
+
+    @Override
+    public void undo() throws Exception {
+        commandManager.undo();
+    }
+
+    @Override
+    public void redo() throws Exception {
+        commandManager.redo();
+    }
+
+    @Override
+    public List<Territory> getMap() {
+        return territories;
+    }
+
+    private void nextPlayer() {
+        currentPlayer = playerOrderList.get(
+                (playerOrderList.indexOf(currentPlayer) + 1) % playerOrderList.size());
+    }
+
+    private void previousPlayer() {
+        currentPlayer = playerOrderList.get(
+                (playerOrderList.indexOf(currentPlayer) - 1) % playerOrderList.size());
+    }
+
+    private enum GameState {SELECT_MAP, SELECT_PLAYERS, INIT_DRAFT, DRAFT, ATTACK, FORTIFY, END}
+
+    private class SelectMap implements Command {
+        private final SingleUIGame gameEngine;
+        private final GameMaps.GameMap selectedGameMap;
+        private final GameState before;
+
+        public SelectMap(SingleUIGame gameEngine, GameMaps.GameMap selectedGameMap) {
+            this.gameEngine = gameEngine;
+            this.selectedGameMap = selectedGameMap;
+            before = gameEngine.gameState;
+            assert before == GameState.INIT_DRAFT;
+        }
+
+        @Override
+        public void execute() throws IllegalExecutionException {
+            gameEngine.territories = selectedGameMap.getConfiguredTerritories();
+            gameEngine.gameState = GameState.SELECT_PLAYERS;
+            gameEngine.ui.update();
+            assert before != gameEngine.gameState;
+        }
+
+        @Override
+        public void undo() throws IllegalUndoException {
+            gameEngine.gameState = before;
+            gameEngine.territories = null;
+        }
+    }
+
+    private class SetPlayers implements Command {
+
+        private final SingleUIGame game;
+        private final List<Player> list;
+        private final GameState before;
+
+        public SetPlayers(SingleUIGame game, List<Player> list) {
+            this.game = game;
+            this.list = list;
+            before = game.gameState;
+            assert before == GameState.SELECT_PLAYERS;
+        }
+
+        @Override
+        public void execute() throws IllegalExecutionException {
+            game.playerOrderList = list;
+            game.currentPlayer = list.get(0);
+            game.gameState = GameState.INIT_DRAFT;
+            game.ui.update();
+        }
+
+        @Override
+        public void undo() throws IllegalUndoException {
+            game.playerOrderList = null;
+            game.currentPlayer = null;
+            game.gameState = before;
+            game.ui.update();
+        }
+    }
+
+    private class DraftOneInit implements Command {
+        private final Territory territory;
+        private final Player player;
+        private final Command draftOne;
+        private final Player previousOwner;
+        private SingleUIGame game;
+
+        public DraftOneInit(SingleUIGame game, Territory territory, Player player) {
+            this.game = game;
+            this.territory = territory;
+            this.player = player;
+            previousOwner = territory.getControlledBy();
+            draftOne = new Territory.DraftOneArmy(territory);
+        }
+
+        @Override
+        public void execute() throws IllegalExecutionException {
+            if (previousOwner != Territory.NoOwner && previousOwner != player)
+                throw new IllegalExecutionException(new OtherPlayerControlsTerritoryException(player, territory));
+            draftOne.execute();
+            territory.setControlledBy(player);
+            game.nextPlayer();
+            game.ui.update();
+        }
+
+        @Override
+        public void undo() throws IllegalUndoException {
+            draftOne.undo();
+            territory.setControlledBy(previousOwner);
+            game.previousPlayer();
+            game.ui.update();
+        }
+    }
+
+    private class OtherPlayerControlsTerritoryException extends Throwable {
+        public final Player player;
+        public final Territory territory;
+
+        public OtherPlayerControlsTerritoryException(Player player, Territory territory) {
+            super("There was an attempt by " + player.getName() + " to draft to " +
+                    territory.getName() + ", which is already controlled by " + territory.getControlledBy().getName() + ".");
+            this.player = player;
+            this.territory = territory;
+        }
+    }
+}
