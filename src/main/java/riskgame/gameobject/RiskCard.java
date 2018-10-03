697bd1bb21d87880b6614305522c2fe3bcd296f8
evelyngalvan <lgalvanevelyn@gmail.com>
2018-10-03 18:38:01 -0500

    command to turn in risk cards - in progress modified/updated/added to Player class fields and methods created method to check whether the cards being turned in are a valid set of cards to trade in for armies

--- src/main/java/riskgame/gameobject/RiskCard.java
+++ src/main/java/riskgame/gameobject/RiskCard.java
@@ -1,19 +1,43 @@
 package riskgame.gameobject;
 
+import riskgame.gameobject.player.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RiskCard {
-    enum RISK_CARD_TYPE {
-        INFANTRY, CAVALRY, ARTILLERY
+    public enum RISK_CARD_TYPE {
+        INFANTRY, CAVALRY, ARTILLERY, WILD
     }
-//this assures that each card can only be of one type
+    //this assures that each card can only be of one type
+    final RISK_CARD_TYPE type;
+    Territory territory;
+    Player cardOwner;
+
     public RiskCard(Territory territory, RISK_CARD_TYPE type) {
         this.territory = territory;
         this.type = type;
+        this.cardOwner = null;
     }
 
-    final RISK_CARD_TYPE type;
-    Territory territory;
+    public RISK_CARD_TYPE getType(){ return this.type;}
+    public Player getCardOwner(){return this.cardOwner;}
+    public void setCardOwner(Player cardOwner){this.cardOwner = cardOwner;}
 
-    RiskCard(RISK_CARD_TYPE type) {
-        this.type = type;
+    public static class IllegalCardTradeException extends  Exception{
+        public IllegalCardTradeException() {
+            super("");
+        }
     }
+
+    public static class didntTurnInThreeCardsException extends Exception{
+        public didntTurnInThreeCardsException(){
+            super("Must trade in 3 cards at a time.");
+        }
+    }
+
+
+
+
 }
