9217c067fc6a80cf6a99aa756170834e79e77c51
evelyngalvan <lgalvanevelyn@gmail.com>
2018-10-03 19:33:40 -0500

    added resources root, added log4j .properties file added Log class

---
+++ src/main/java/riskgame/Log.java
@@ -0,0 +1,8 @@
+package riskgame;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log {
+    private final Logger logger = LogManager.getLogger(Log.class.getName());
+}
