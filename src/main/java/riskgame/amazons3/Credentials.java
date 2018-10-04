4ffed9e5578bf3ea4b17f5dc89800dd30563edbc
Joshua <joshchan17@gmail.com>
2018-10-04 17:35:12 -0500

    added

---
+++ src/main/java/riskgame/amazons3/Credentials.java
@@ -0,0 +1,12 @@
+package riskgame.amazons3;
+
+public class Credentials {
+
+    public static final String access_key_id= "AKIAJK3BFXWUSDNXKIOA";
+
+    public static final String secret_access_key ="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";
+
+
+
+}
+
