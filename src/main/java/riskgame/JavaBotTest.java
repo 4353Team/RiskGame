31a56e2a3eb429bd6346372610a97d619afa36f1
Joshua <joshchan17@gmail.com>
2018-11-05 13:40:44 -0600

    Added chatbot that will echo messages. Working on adding commands

---
+++ src/main/java/riskgame/JavaBotTest.java
@@ -0,0 +1,23 @@
+package riskgame;
+
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class JavaBotTest {
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new JavaRiskGameBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
